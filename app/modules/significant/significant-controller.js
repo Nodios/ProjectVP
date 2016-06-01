a7de50dcf947b9d148fefae0b152a1f06b8b397e
Dino Repac <drepac@mono-software.com>
2016-06-01 16:17:31 +0200

    strongest eq in past 30 days added

--- app/modules/significant/significant-controller.js
+++ app/modules/significant/significant-controller.js
@@ -20,29 +20,42 @@
       getPastMonth();
 
       function getPastMonth(){
-            $scope.hasLoadedData = false;
-            data = EarthquakeFactory.getEarthquakesPastMonth();
-            data.then(function(result) {
-               // display data when avaliable
-               for(var eq in result.data.features){
-                  var data = result.data.features[eq];
-                  if(data.properties.mag > 6){
-                      $scope.eqArray.push(data);
-                  }
-                  if(data.properties.tsunami > 0){
-                    $scope.eqWithTsunamiWarningArray.push(data);
-                  }
-               }
+        $scope.hasLoadedData = false;
+        data = EarthquakeFactory.getEarthquakesPastMonth();
+        data.then(function(result) {
+           // display data when avaliable
+           for(var eq in result.data.features){
+              var data = result.data.features[eq];
+              if(data.properties.mag > 6){
+                  $scope.eqArray.push(data);
+              }
+              if(data.properties.tsunami > 0){
+                $scope.eqWithTsunamiWarningArray.push(data);
+              }
+           }
 
-               $scope.status = result.data.metadata.status;
-               $scope.title = result.data.metadata.title;
-               $scope.count = result.data.metadata.count;
+           $scope.maxMagnitudeEearthquake = getMaxMagnitude($scope.eqArray);
 
-               $scope.earthquakes = result.data.features;
-               $scope.url = result.data.metadata.url;
+           $scope.status = result.data.metadata.status;
+           $scope.title = result.data.metadata.title;
+           $scope.count = result.data.metadata.count;
 
-               $scope.hasLoadedData = true;
-            });
+           $scope.earthquakes = result.data.features;
+           $scope.url = result.data.metadata.url;
+
+           $scope.hasLoadedData = true;
+        });
+      }
+
+      function getMaxMagnitude(array){
+        var len = array.length, max = -Infinity, output;
+        while(len--){
+          if(array[len].properties.mag > max){
+            max = array[len].properties.mag
+            output = array[len];
+          }
         }
+        return output;
+      }
     }
 })();
